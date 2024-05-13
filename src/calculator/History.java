df7e629c1efde8b057aeb3cddc79fd699355598d
Eduardo Angulo <edangulo@uninorte.edu.co>
2024-05-13 00:09:16 -0500

    Initial commit

---
+++ src/calculator/History.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author edangulo
+ */
+public class History {
+    
+    private ArrayList<Operation> operations;
+
+    public History() {
+        this.operations = new ArrayList<>();
+    }
+    
+    public void addOperation(Operation operation) {
+        this.operations.add(operation);
+    }
+
+    public ArrayList<Operation> getOperations() {
+        return operations;
+    }
+    
+}

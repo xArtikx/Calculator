cd94af0b948d275549168130e4f43b4dd839f274
xArtikx <162181276+xArtikx@users.noreply.github.com>
2024-05-26 08:13:47 -0500

    Tercer Commit de Andrés Forero

--- src/core/controllers/operation/OperationController.java
+++ src/core/controllers/operation/OperationController.java
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
->>>>>>> 572cc9cc8cd8126d79b68c12b0fdc84569a4cbb5
 package core.controllers.operation;
 
 import core.controllers.utils.Response;
@@ -15,10 +8,7 @@ import core.models.operation.Operation;
  *
  * @author andre
  */
-<<<<<<< HEAD
-=======
 
->>>>>>> 572cc9cc8cd8126d79b68c12b0fdc84569a4cbb5
 //Se usa esta interfaz para cada una de las operaciones
 //Contiene los getters solamente y todas las operaciones la implementan
 public interface OperationController {
@@ -28,10 +18,5 @@ public interface OperationController {
 
     Operation
             getOperation(double number1, double number2);
-<<<<<<< HEAD
-=======
 
-    String
-            getOperationResult(double number1, double number2);
->>>>>>> 572cc9cc8cd8126d79b68c12b0fdc84569a4cbb5
 }

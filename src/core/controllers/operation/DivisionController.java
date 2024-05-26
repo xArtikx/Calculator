4b532e077dc867e59b4d3fb01415d41d44e38d23
xArtikx <162181276+xArtikx@users.noreply.github.com>
2024-05-26 08:16:17 -0500

    Sexto Commit de Andrés Forero

--- src/core/controllers/operation/DivisionController.java
+++ src/core/controllers/operation/DivisionController.java
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
->>>>>>> 572cc9cc8cd8126d79b68c12b0fdc84569a4cbb5
 package core.controllers.operation;
 
 import core.controllers.utils.Response;
@@ -28,19 +21,5 @@ public class DivisionController implements OperationController {
         return new Division(number1, number2);
     }
 
-<<<<<<< HEAD
-   
-=======
-    /**
-     *
-     * @param number1
-     * @param number2
-     * @return
-     */
-    @Override
-    public String getOperationResult(double number1, double number2) {
-        Division mu = new Division(number1, number2);
-        return mu.getResult();
-    }
->>>>>>> 572cc9cc8cd8126d79b68c12b0fdc84569a4cbb5
+
 }

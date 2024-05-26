8764d2f22e35ae433f92bf3e94b0dcb2fcebb6de
juan fernandez <fjuanc@uninorte.edu.co>
2024-05-25 22:49:05 -0500

    Test

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculator
+Juan Camilo Fernández Barrios
+Andrés 
---
+++ src/core/models/operation/Division.java
@@ -0,0 +1,16 @@
+package core.models.operation;
+
+
+
+/**
+ *
+ * @author juank
+ */
+public class Division extends Operation {
+
+    public Division(double number1, double number2) {
+        super(number1, number2);     
+        result = String.format("%.3f", number1/number2).replaceAll(",", ".");
+        operator = "/";
+    }
+}

2891cc5b0c5aec61d18450f00bea5f55f07d524b
xArtikx <162181276+xArtikx@users.noreply.github.com>
2024-05-26 08:14:45 -0500

    Cuarto Commit de Andrés Forero

--- src/core/controllers/operation/MultiplicationController.java
+++ src/core/controllers/operation/MultiplicationController.java
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
->>>>>>> 572cc9cc8cd8126d79b68c12b0fdc84569a4cbb5
 package core.controllers.operation;
 
 import core.controllers.utils.Response;
@@ -28,13 +21,4 @@ public class MultiplicationController implements OperationController {
         return new Multiplication(number1, number2);
     }
 
-<<<<<<< HEAD
-  
-=======
-    @Override
-    public String getOperationResult(double number1, double number2) {
-        Multiplication mu = new Multiplication(number1, number2);
-        return mu.getResult();
-    }
->>>>>>> 572cc9cc8cd8126d79b68c12b0fdc84569a4cbb5
 }
